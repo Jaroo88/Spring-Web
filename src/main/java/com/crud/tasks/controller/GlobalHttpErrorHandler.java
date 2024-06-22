ef31f0d9afdc6a7ad9bcf237bb6cebb44fd27650
jaroo88 <jaroslaw.kolacz@op.pl>
2024-06-22 15:16:43 +0200

    Zadanie 18.3

---
+++ src/main/java/com/crud/tasks/controller/GlobalHttpErrorHandler.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
+@ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleTaskNotFoundException(TaskNotFoundException exception) {
+        return new ResponseEntity<>("task with given id doesn't exist", HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,27 +1,40 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 
 @Service
 @RequiredArgsConstructor
 public class DbService {
 
-    private final TaskRepository repository;
+    @Autowired
+    private TaskRepository repository;
 
     public List<Task> getAllTasks() {
-    return (List<Task>) repository.findAll();
+        return repository.findAll();
+    }
 
+    public Task saveTask(final Task task) {
+        return repository.save(task);
     }
-    public Optional<Task> getTaskById( Long id) {
-        return repository.findById(id);
+
+    public Task getTask(final Long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
     }
+    public void deleteTask(final Long taskId) {
+        repository.deleteById(taskId);
+    }
+
 }
-//
+
 
