8a6f4fc9cb5aac89b7ac86230dd9494192eac027
jaroo88 <jaroslaw.kolacz@op.pl>
2024-06-29 10:06:28 +0200

    test

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/tasks")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class TaskController {
     private final DbService service;
     private final TaskMapper taskMapper;
@@ -35,7 +36,7 @@ public class TaskController {
     }
     @DeleteMapping(value = "{taskId}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
-        service.deleteTask(taskId);  // Dodajemy wywołanie metody usuwającej w serwisie
+        service.deleteTask(taskId);
         return ResponseEntity.ok().build();
 
     }
